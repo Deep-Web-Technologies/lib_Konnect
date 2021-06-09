f5f9658945fb5cedc240fc4348c809008aaef7a6
Alex O'Connor <alexoconnor212@gmail.com>
2021-06-09 13:00:06 +0100

    KOMAINT-3375-UpdateIterators

--- src/Iterator/PSCs.php
+++ src/Iterator/PSCs.php
@@ -12,7 +12,36 @@ class PSCs extends IttModelAbstract
     public function getByName(string $strName) : ?ModelPSC
     {
         foreach ($this as $modelPSC) {
-            if (strtolower($modelPSC->getName()) === strtolower($strName)) {
+            $strModelName = preg_replace('/\s+/', ' ', $modelPSC->getName());
+            $strName = preg_replace('/\s+/', ' ', $strName);
+
+            if (strtolower($strModelName) === strtolower($strName)) {
+                return $modelPSC;
+            }
+
+            $arrNames = explode(' ', $strName);
+            $bFirstNameMatch = false;
+            $bLastNameMatch = false;
+
+            foreach ($arrNames as $strNamePart) {
+                $arrFirstName = explode(' ', $modelPSC->getFirstName());
+
+                $strFirstName = $arrFirstName[0];
+
+                if (
+                    strtolower($strNamePart) ===
+                    strtolower($strFirstName)
+                ) {
+                    $bFirstNameMatch = true;
+                } else if (
+                    strtolower($strNamePart) ===
+                    strtolower($modelPSC->getLastName())
+                ) {
+                    $bLastNameMatch = true;
+                }
+            }
+
+            if ($bFirstNameMatch && $bLastNameMatch) {
                 return $modelPSC;
             }
         }
